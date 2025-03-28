7c6ac6fc4eac220621295f13db96f583a9d8f8a9
Rafael Corral <rcorralx@gmail.com>
2025-03-27 21:02:28 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const Gun = require('gun')
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.use(Gun.serve)
+
+
+const server = app.listen(port, () => {
+  console.log(`Gun server running on port ${port}`)
+})
+
+
+Gun({ web: server })
